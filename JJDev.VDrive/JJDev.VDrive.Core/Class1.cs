d4517ec82573b2e275be2c0376b757cfadde5c16
Unknown <jbrink386@gmail.com>
2017-03-08 22:16:02 +0200

    Updates
    Added unit test project JJDev.VDrive.UnitTests
    Added unmanaged component imports

--- JJDev.VDrive/JJDev.VDrive.Core/Class1.cs
+++ JJDev.VDrive/JJDev.VDrive.Core/Class1.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace JJDev.VDrive.Core
 {
     public class Class1
     {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool DefineDosDevice(int flags, string devname, string path);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int QueryDosDevice(string devname, StringBuilder buffer, int bufSize);
     }
 }
