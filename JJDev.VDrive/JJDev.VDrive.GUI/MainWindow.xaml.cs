37be362c50fcb770097623b2d41623707cccc4d1
Brinkies <jbrink386@gmail.com>
2017-04-04 20:39:03 +0200

    Update GUI
    Added simple map/unmap logic
    Added storage directory validation

--- JJDev.VDrive/JJDev.VDrive.GUI/MainWindow.xaml.cs
+++ JJDev.VDrive/JJDev.VDrive.GUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using JJDev.VDrive.Core;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,54 +23,64 @@ namespace JJDev.VDrive.GUI
     public partial class MainWindow : Window
     {
         private bool loading = true;
+        private string basePath = @"C:\Virtual Drives";
 
         public MainWindow()
         {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
-            DriveSelectionComboBox.SelectionChanged += DriveSelectionComboBox_SelectionChanged;     
+            DriveActionButton.Click += DriveActionButton_Click;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            //TestDriveMap();
-            //TestDriveUnMap();
-            LoadDrives();
-
-            loading = false;
+            LoadDrives();            
         }
 
-        private void DriveSelectionComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (loading)
-            {
+        private void DriveActionButton_Click(object sender, RoutedEventArgs e)
+        {            
+            var drive = DriveSelectionComboBox.SelectedItem.ToString();
+            var path = ValidateDirectory(basePath, drive.Replace(":", string.Empty));
+
+            if (DriveMaster.DriveValid(drive))
+            {                
+                DriveMaster.UnMapDrive(drive);
+
+                // TODO:
+                // Hanle files in use.
+                // Encrypt directory into unreadable file
                 return;
             }
 
-            
+            DriveMaster.MapDrive(drive, path);
         }
 
-
-        public void TestDriveMap()
-        {
-            var drives = DriveMaster.GenerateDriveLetters();
-            DriveMaster.MapDrive(drives[24], @"C:\Virtual Drives");
-        }
-
-        public void TestDriveUnMap()
-        {
-            var drives = DriveMaster.GenerateDriveLetters();
-            DriveMaster.UnMapDrive(drives[24]);
-        }
 
         public void LoadDrives()
         {
-            //var drives = DriveMaster.GenerateDriveLetters();
-            var drives = DriveMaster.AvailableDrives(false);
-            //var drives = DriveMaster.AvailableDrives(true);
-            drives.ForEach(d => DriveSelectionComboBox.Items.Add(d));
+            var availableDrives = DriveMaster.GenerateDriveLetters();
+            var actualDrives = DriveMaster.AvailableDrives(false);
+
+            for (int i = 0; i < availableDrives.Count; i++)
+            {
+                if (actualDrives.Contains(availableDrives[i]))
+                {
+                    availableDrives.Remove(availableDrives[i]);
+                    i--;
+                }
+            }
+
+            availableDrives.ForEach(d => DriveSelectionComboBox.Items.Add(d));
         }
 
-
+        private string ValidateDirectory(string basePath, string directoryName)
+        {
+            var path = System.IO.Path.Combine(basePath, directoryName);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            return path;
+        }
     }
 }
