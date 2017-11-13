a9c6dc83728d31d381556cc685b0036dc93e0fea
Unknown <jbrink386@gmail.com>
2017-11-13 19:30:47 +0200

    Added RC2, Rijndael and TripleDES tests

--- JJDev.VDrive/JJDev.VDrive.Tests/CipherTests.cs
+++ JJDev.VDrive/JJDev.VDrive.Tests/CipherTests.cs
@@ -27,16 +27,16 @@ namespace JJDev.VDrive.Tests
           cipher.IV = new byte[] { 228, 26, 220, 137, 168, 134, 150, 239 };
           break;
         case SymmetricCipherType.RC2:
-          cipher.Key = null;
-          cipher.IV = null;
+          cipher.Key = new byte[] { 110, 118, 139, 107, 92, 75, 250, 148, 166, 165, 22, 233, 74, 131, 105, 140 };
+          cipher.IV = new byte[] { 210, 64, 11, 132, 61, 29, 73, 253 };
           break;
         case SymmetricCipherType.Rijndael:
-          cipher.Key = null;
-          cipher.IV = null;
+          cipher.Key = new byte[] { 187, 136, 26, 38, 169, 83, 131, 237, 205, 215, 81, 219, 166, 220, 93, 211, 25, 114, 19, 246, 18, 219, 176, 197, 18, 175, 95, 183, 9, 1, 241, 222 };
+          cipher.IV = new byte[] { 186, 186, 148, 241, 211, 29, 97, 52, 254, 235, 110, 109, 5, 134, 76, 95 };
           break;
         case SymmetricCipherType.TripleDES:
-          cipher.Key = null;
-          cipher.IV = null;
+          cipher.Key = new byte[] { 50, 181, 3, 3, 107, 23, 19, 186, 32, 123, 183, 16, 199, 11, 198, 94, 252, 75, 153, 78, 26, 63, 215, 132 };
+          cipher.IV = new byte[] { 135, 1, 207, 137, 109, 70, 219, 10 };
           break;
         default:
           break;
@@ -116,5 +116,82 @@ namespace JJDev.VDrive.Tests
 
       Assert.AreEqual(expected, actual);
     }
-  }
+
+        [Test]
+        public void TestRC2Encode_GivenInput_ShouldReturnByteArray()
+        {
+            var expected = new byte[] { 220, 127, 16, 101, 141, 35, 33, 61, 11, 159, 81, 239, 46, 106, 162, 107 };
+            var cipher = Substitute.For<SymmetricAlgorithmCipher>();            
+            SetupKeyIV(cipher, SymmetricCipherType.RC2);
+
+            var actual = cipher.Encode(SymmetricCipherType.RC2, "content777");            
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestRC2Decode_GivenInput_ShouldReturnByteArray()
+        {
+            var expected = "content777";
+            var cipher = Substitute.For<SymmetricAlgorithmCipher>();
+            SetupKeyIV(cipher, SymmetricCipherType.RC2);
+
+            var actual = cipher.Decode(SymmetricCipherType.RC2,
+              new byte[] { 220, 127, 16, 101, 141, 35, 33, 61, 11, 159, 81, 239, 46, 106, 162, 107 });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestRijndaelEncode_GivenInput_ShouldReturnByteArray()
+        {
+            var expected = new byte[] { 83, 59, 165, 116, 117, 105, 24, 154, 63, 129, 250, 104, 15, 145, 201, 93 };
+            var cipher = Substitute.For<SymmetricAlgorithmCipher>();            
+            SetupKeyIV(cipher, SymmetricCipherType.Rijndael);
+
+            var actual = cipher.Encode(SymmetricCipherType.Rijndael, "content777");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestRijndaelDecode_GivenInput_ShouldReturnByteArray()
+        {
+            var expected = "content777";
+            var cipher = Substitute.For<SymmetricAlgorithmCipher>();
+            SetupKeyIV(cipher, SymmetricCipherType.Rijndael);
+
+            var actual = cipher.Decode(SymmetricCipherType.Rijndael,
+              new byte[] { 83, 59, 165, 116, 117, 105, 24, 154, 63, 129, 250, 104, 15, 145, 201, 93 });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestTripleDESEncode_GivenInput_ShouldReturnByteArray()
+        {
+            var expected = new byte[] { 153, 126, 106, 58, 25, 174, 201, 147, 114, 110, 12, 12, 225, 232, 89, 255 };
+            var cipher = Substitute.For<SymmetricAlgorithmCipher>();            
+            SetupKeyIV(cipher, SymmetricCipherType.TripleDES);
+
+            var actual = cipher.Encode(SymmetricCipherType.TripleDES, "content777");
+            var sb = new StringBuilder();
+            actual.ToList().ForEach(x => sb.Append(x + ","));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestTripleDESDecode_GivenInput_ShouldReturnByteArray()
+        {
+            var expected = "content777";
+            var cipher = Substitute.For<SymmetricAlgorithmCipher>();
+            SetupKeyIV(cipher, SymmetricCipherType.TripleDES);
+
+            var actual = cipher.Decode(SymmetricCipherType.TripleDES,
+              new byte[] { 153, 126, 106, 58, 25, 174, 201, 147, 114, 110, 12, 12, 225, 232, 89, 255 });
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
 }
