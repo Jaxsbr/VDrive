4083b2f3e0fda1fb1c3d2fab00316d45d8528def
Jaco Brink <Jaco.Brink@derivco.co.za>
2017-11-13 14:46:53 +0200

    Update factory and DES tests

--- JJDev.VDrive/JJDev.VDrive.Core/Ciphers/SymmetricCipherFactory.cs
+++ JJDev.VDrive/JJDev.VDrive.Core/Ciphers/SymmetricCipherFactory.cs
@@ -37,5 +37,55 @@ namespace JJDev.VDrive.Core.Ciphers
           throw new InvalidOperationException($"Invalid Symmetric Cipher Type Provided: {type}");
       }
     }
+
+    public static void PopulateKeyIV(ICipher cipher, SymmetricCipherType type)
+    {
+      switch (type)
+      {
+        case SymmetricCipherType.Aes:
+          var aes = Aes.Create();
+          aes.GenerateKey();
+          aes.GenerateIV();
+          cipher.Key = aes.Key;
+          cipher.IV = aes.IV;
+          break;
+        case SymmetricCipherType.DES:
+          var des = DES.Create();
+          des.GenerateKey();
+          des.GenerateIV();
+          cipher.Key = des.Key;
+          cipher.IV = des.IV;
+          break;
+        case SymmetricCipherType.RC2:
+          var rc2 = RC2.Create();
+          rc2.GenerateKey();
+          rc2.GenerateIV();
+          cipher.Key = rc2.Key;
+          cipher.IV = rc2.IV;
+          break;
+        case SymmetricCipherType.Rijndael:
+          var rijndael = Rijndael.Create();
+          rijndael.GenerateKey();
+          rijndael.GenerateIV();
+          cipher.Key = rijndael.Key;
+          cipher.IV = rijndael.IV;
+          break;
+        case SymmetricCipherType.TripleDES:
+          var tripleDES = TripleDES.Create();
+          tripleDES.GenerateKey();
+          tripleDES.GenerateIV();
+          cipher.Key = tripleDES.Key;
+          cipher.IV = tripleDES.IV;
+          break;
+        default:
+          throw new InvalidOperationException($"Invalid Symmetric Cipher Type Provided: {type}");
+      }
+
+      // TEMP:
+      var sb = new StringBuilder();
+      cipher.Key.ToList().ForEach(x => sb.Append(x + ","));
+      sb.Clear();
+      cipher.IV.ToList().ForEach(x => sb.Append(x + ","));
+    }    
   }
 }
--- JJDev.VDrive/JJDev.VDrive.Tests/CipherTests.cs
+++ JJDev.VDrive/JJDev.VDrive.Tests/CipherTests.cs
@@ -13,25 +13,41 @@ namespace JJDev.VDrive.Tests
 {
   [TestFixture]
   public class CipherTests
-  {
-    private static ICipher Setup_GetCipher()
-    {
-      var cipher = Substitute.For<ICipher>();
-      Setup_AddKeyAndIV(cipher);
-      return cipher;
-    }
-
-    private static void Setup_AddKeyAndIV(ICipher cipher)
-    {
-      cipher.Key = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
-      cipher.IV = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
+  {    
+    private void SetupKeyIV(ICipher cipher, SymmetricCipherType symmetricCipherType)
+    {      
+      switch (symmetricCipherType)
+      {
+        case SymmetricCipherType.Aes:
+          cipher.Key = new byte[] { 105, 195, 252, 185, 2, 140, 51, 126, 104, 229, 79, 123, 212, 18, 202, 2, 110, 30, 207, 111, 0, 244, 173, 234, 220, 14, 253, 178, 156, 52, 214, 127 };
+          cipher.IV = new byte[] { 8, 68, 137, 198, 127, 127, 18, 72, 241, 104, 126, 253, 191, 17, 44, 132 };
+          break;
+        case SymmetricCipherType.DES:
+          cipher.Key = new byte[] { 193, 114, 251, 87, 29, 105, 15, 251 };
+          cipher.IV = new byte[] { 228, 26, 220, 137, 168, 134, 150, 239 };
+          break;
+        case SymmetricCipherType.RC2:
+          cipher.Key = null;
+          cipher.IV = null;
+          break;
+        case SymmetricCipherType.Rijndael:
+          cipher.Key = null;
+          cipher.IV = null;
+          break;
+        case SymmetricCipherType.TripleDES:
+          cipher.Key = null;
+          cipher.IV = null;
+          break;
+        default:
+          break;
+      }
     }
 
     [Test]
     public void TestEncode_GivenEmptyInput_ShouldReturnEmptyByteArray()
     {
       var expected = new byte[0];
-      var cipher = Setup_GetCipher();
+      var cipher = Substitute.For<ICipher>();
       cipher.Encode(Arg.Any<SymmetricCipherType>(), Arg.Any<string>()).Returns(new byte[0]);
 
       var actual = cipher.Encode(SymmetricCipherType.Aes, string.Empty);
@@ -43,7 +59,7 @@ namespace JJDev.VDrive.Tests
     public void TestDecode_GivenEmptyInput_ShouldReturnEmptyString()
     {
       var expected = string.Empty;
-      var cipher = Setup_GetCipher();
+      var cipher = Substitute.For<ICipher>();
       cipher.Decode(Arg.Any<SymmetricCipherType>(), Arg.Any<byte[]>()).Returns(string.Empty);
 
       var actual = cipher.Decode(SymmetricCipherType.Aes, new byte[0]);
@@ -54,9 +70,9 @@ namespace JJDev.VDrive.Tests
     [Test]
     public void TestAesEncode_GivenInput_ShouldReturnByteArray()
     {
-      var expected = new byte[] { 182, 83, 183, 192, 122, 91, 118, 135, 243, 84, 60, 97, 134, 141, 227, 211 };
+      var expected = new byte[] { 51, 218, 85, 191, 9, 115, 231, 102, 129, 101, 85, 25, 233, 160, 16, 84 };
       var cipher = Substitute.For<SymmetricAlgorithmCipher>();
-      Setup_AddKeyAndIV(cipher);
+      SetupKeyIV(cipher, SymmetricCipherType.Aes);
 
       var actual = cipher.Encode(SymmetricCipherType.Aes, "content777");
 
@@ -68,10 +84,35 @@ namespace JJDev.VDrive.Tests
     {
       var expected = "content777";
       var cipher = Substitute.For<SymmetricAlgorithmCipher>();
-      Setup_AddKeyAndIV(cipher);
+      SetupKeyIV(cipher, SymmetricCipherType.Aes);
 
       var actual = cipher.Decode(SymmetricCipherType.Aes, 
-        new byte[] { 182, 83, 183, 192, 122, 91, 118, 135, 243, 84, 60, 97, 134, 141, 227, 211 });
+        new byte[] { 51, 218, 85, 191, 9, 115, 231, 102, 129, 101, 85, 25, 233, 160, 16, 84 });
+
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void TestDESEncode_GivenInput_ShouldReturnByteArray()
+    {
+      var expected = new byte[] { 78, 194, 203, 79, 146, 183, 225, 222, 136, 18, 91, 166, 154, 219, 60, 204 };
+      var cipher = Substitute.For<SymmetricAlgorithmCipher>();
+      SetupKeyIV(cipher, SymmetricCipherType.DES);
+
+      var actual = cipher.Encode(SymmetricCipherType.DES, "content777");
+
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void TestDESDecode_GivenInput_ShouldReturnByteArray()
+    {
+      var expected = "content777";
+      var cipher = Substitute.For<SymmetricAlgorithmCipher>();
+      SetupKeyIV(cipher, SymmetricCipherType.DES);
+
+      var actual = cipher.Decode(SymmetricCipherType.DES,
+        new byte[] { 78, 194, 203, 79, 146, 183, 225, 222, 136, 18, 91, 166, 154, 219, 60, 204 });
 
       Assert.AreEqual(expected, actual);
     }
