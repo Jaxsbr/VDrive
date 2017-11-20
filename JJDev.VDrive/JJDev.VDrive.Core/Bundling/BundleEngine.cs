6a4bc035cec487b5c6aab5e8d9f5b48efba1dbbc
Unknown <jbrink386@gmail.com>
2017-11-20 22:06:44 +0200

    refactor(drivemanagement): Corrected indenting

--- JJDev.VDrive/JJDev.VDrive.Core/Bundling/BundleEngine.cs
+++ JJDev.VDrive/JJDev.VDrive.Core/Bundling/BundleEngine.cs
@@ -8,46 +8,46 @@ using System.Threading.Tasks;
 
 namespace JJDev.VDrive.Core.Bundling
 {
-  public class BundleEngine : IBundleEngine
-  {
-    // [Compress]
-    // Map hirarchy (serialize able object)
-    // Append to stream
-    // Append encoded file content to stream, disregard hirarchy
-    // Write stream to file
-
-    public object Compress(string source, string destination)
+    public class BundleEngine : IBundleEngine
     {
-      var hierarchyMap = GetHierarchy(source);      
-      var manifest = hierarchyMap.ToString();
+        // [Compress]
+        // Map hirarchy (serialize able object)
+        // Append to stream
+        // Append encoded file content to stream, disregard hirarchy
+        // Write stream to file
 
-      return null;
+        public object Compress(string source, string destination)
+        {
+            var hierarchyMap = GetHierarchy(source);
+            var manifest = hierarchyMap.ToString();
+
+            return null;
+        }
+
+        private HierarchyMap GetHierarchy(string source)
+        {
+            var hierarchyMap = new HierarchyMap() { Path = source, IsFile = false };
+            var files = SystemIO.GetFiles(source);
+            var folders = SystemIO.GetFolders(source);
+
+            files.ForEach(file => { hierarchyMap.Hierarchies.Add(new HierarchyMap() { Path = file, IsFile = true }); });
+            folders.ForEach(folder =>
+            {
+                hierarchyMap.Hierarchies.Add(GetHierarchy(folder));
+            });
+
+            return hierarchyMap;
+        }
+
+
+        // [Decompress]
+        // Read content into stream
+        // Read hirarchy map part and construct object
+        // Read remaining stream into flat list of file content
+        // Generate original directory structure at destination
+        public object Decompress(string source, string destination)
+        {
+            throw new NotImplementedException();
+        }
     }
-
-    private HierarchyMap GetHierarchy(string source)
-    {
-      var hierarchyMap = new HierarchyMap() { Path = source, IsFile = false };
-      var files = SystemIO.GetFiles(source);
-      var folders = SystemIO.GetFolders(source);
-
-      files.ForEach(file => { hierarchyMap.Hierarchies.Add(new HierarchyMap() { Path = file, IsFile = true }); });
-      folders.ForEach(folder => 
-      {                
-        hierarchyMap.Hierarchies.Add(GetHierarchy(folder));        
-      });
-
-      return hierarchyMap;
-    }
-
-
-    // [Decompress]
-    // Read content into stream
-    // Read hirarchy map part and construct object
-    // Read remaining stream into flat list of file content
-    // Generate original directory structure at destination
-    public object Decompress(string source, string destination)
-    {
-      throw new NotImplementedException();
-    }
-  }
 }
--- JJDev.VDrive/JJDev.VDrive.Tests/DriveTests.cs
+++ JJDev.VDrive/JJDev.VDrive.Tests/DriveTests.cs
@@ -8,69 +8,69 @@ using System.Threading.Tasks;
 
 namespace JJDev.VDrive.Tests
 {
-  [TestFixture]
-  public class DriveTests
-  {
-    [TestCase("E:")]
-    [TestCase("c:")]
-    public void IsDriveInUseTest_GivenValidDriveLetter_ShouldReturnTrue(string input)
+    [TestFixture]
+    public class DriveTests
     {
-      var expected = true;      
+        [TestCase("E:")]
+        [TestCase("c:")]
+        public void IsDriveInUseTest_GivenValidDriveLetter_ShouldReturnTrue(string input)
+        {
+            var expected = true;
 
-      var actual = DriveMaster.IsDriveInUse(input);
+            var actual = DriveMaster.IsDriveInUse(input);
 
-      Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(@"c:\")]
+        [TestCase("asdv:")]
+        [TestCase("1:")]
+        [TestCase("z:")]
+        public void IsDriveInUseTest_GivenInvalidDriveLetter_ShouldReturnFalse(string input)
+        {
+            var expected = false;
+
+            var actual = DriveMaster.IsDriveInUse(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(@"c:\")]
+        [TestCase("asdv:")]
+        [TestCase("1:")]
+        public void IsDriveLetterValidTest_GivenInvalidDriveLetter_ShouldReturnFalse(string input)
+        {
+            var expected = false;
+
+            var actual = DriveMaster.IsDriveLetterValid(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MountTest_GivenDriveLetter_ShouldReturnTrue()
+        {
+            var expected = true;
+
+            DriveMaster.Mount("z:", @"c:\");
+            var actual = DriveMaster.IsDriveInUse("z:");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void DismountTest_GivenDriveLetter_ShouldReturnTrue()
+        {
+            var expected = true;
+
+            DriveMaster.Dismount("z:");
+            var actual = !DriveMaster.IsDriveInUse("z:");
+
+            Assert.AreEqual(expected, actual);
+        }
     }
-
-    [TestCase("")]
-    [TestCase(" ")]
-    [TestCase(@"c:\")]
-    [TestCase("asdv:")]
-    [TestCase("1:")]
-    [TestCase("z:")]
-    public void IsDriveInUseTest_GivenInvalidDriveLetter_ShouldReturnFalse(string input)
-    {
-      var expected = false;
-
-      var actual = DriveMaster.IsDriveInUse(input);
-
-      Assert.AreEqual(expected, actual);
-    }
-
-    [TestCase("")]
-    [TestCase(" ")]
-    [TestCase(@"c:\")]
-    [TestCase("asdv:")]
-    [TestCase("1:")]
-    public void IsDriveLetterValidTest_GivenInvalidDriveLetter_ShouldReturnFalse(string input)
-    {
-      var expected = false;
-
-      var actual = DriveMaster.IsDriveLetterValid(input);
-
-      Assert.AreEqual(expected, actual);
-    }
-
-    [Test]
-    public void MountTest_GivenDriveLetter_ShouldReturnTrue()
-    {
-      var expected = true;
-
-      DriveMaster.Mount("z:", @"c:\");
-      var actual = DriveMaster.IsDriveInUse("z:");
-
-      Assert.AreEqual(expected, actual);
-    }
-
-    [Test]
-    public void DismountTest_GivenDriveLetter_ShouldReturnTrue()
-    {
-      var expected = true;
-
-      DriveMaster.Dismount("z:");
-      var actual = !DriveMaster.IsDriveInUse("z:");
-
-      Assert.AreEqual(expected, actual);
-    }
-  }
 }
