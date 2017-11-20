b3026dc99062f091a97edad4581e01ebcffb65e8
Unknown <jbrink386@gmail.com>
2017-11-20 22:05:39 +0200

    feat(desktop): Added wpf ui project
    - feature test added mount
    - feature test added dismount
    - feature test added test

---
+++ JJDev.VDrive/JJDev.VDrive.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using JJDev.VDrive.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JJDev.VDrive.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MountButton.Click += MountButton_Click;
+            DismountButton.Click += DismountButton_Click;
+            TestButton.Click += TestButton_Click;
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = DriveMaster.IsDriveInUse("z:");
+            MessageBox.Show($"Z  in use: {result}");
+        }
+
+        private void DismountButton_Click(object sender, RoutedEventArgs e)
+        {
+            DriveMaster.Dismount("z:");
+            MessageBox.Show("Z  Dismounted!");
+        }
+
+        private void MountButton_Click(object sender, RoutedEventArgs e)
+        {
+            DriveMaster.Mount("z:", "C:\test");
+            MessageBox.Show("Z  Mounted!");
+        }
+    }
+}
