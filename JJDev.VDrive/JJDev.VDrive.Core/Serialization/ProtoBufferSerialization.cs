f4460920b455b0b15c07a4775a451214d5314acc
Unknown <jbrink386@gmail.com>
2017-12-09 23:12:35 +0200

    build(serialization): Added protobuff packages

---
+++ JJDev.VDrive/JJDev.VDrive.Core/Serialization/ProtoBufferSerialization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJDev.VDrive.Core.Serialization
+{
+    public class ProtoBufferSerialization : ISerialization
+    {
+        public T Deserialize<T>(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Serialize(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
