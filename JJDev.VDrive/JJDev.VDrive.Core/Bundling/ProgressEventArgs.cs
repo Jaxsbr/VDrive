5f80ed2471d1beba8da6eabe2c4ec3e78706ba64
Jaco Brink <Jaco.Brink@derivco.co.za>
2018-01-04 10:54:53 +0200

    feat(bundling): Percentage calculation added to custom event args

--- JJDev.VDrive/JJDev.VDrive.Core/Bundling/ProgressEventArgs.cs
+++ JJDev.VDrive/JJDev.VDrive.Core/Bundling/ProgressEventArgs.cs
@@ -4,5 +4,30 @@ namespace JJDev.VDrive.Core.Bundling
 {
   public class ProgressEventArgs : EventArgs
   {
+    private readonly int _progressIndex = 0;
+    private readonly int _maxProgress = 0;
+
+
+    private int _percentageCompleted = -1;
+    public int PercentageCompleted
+    {
+      get
+      {
+        if (_percentageCompleted < 0) { _percentageCompleted = _progressIndex / _maxProgress * 100; }
+        return _percentageCompleted;
+      }
+    }
+
+
+    public ProgressEventArgs(int progressIndex, int maxProgress)
+    {
+      if (progressIndex < 0 || maxProgress < 0 || progressIndex > maxProgress)
+      {
+        throw new InvalidOperationException("Invalid input");
+      }      
+
+      _progressIndex = progressIndex;
+      _maxProgress = maxProgress;
+    }
   }
 }
